261fbb657eda86601f36de5ff93c9170e0e70a4c
motoki317 <motoki317@gmail.com>
2020-02-22 20:40:05 +0900

    add long version partition_point

--- src/main/BinarySearch.java
+++ src/main/BinarySearch.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
 
 class BinarySearch {
     /**
@@ -13,6 +14,19 @@ class BinarySearch {
      * @param last the partitioned range of elements to examine
      * @return The iterator past the end of the first partition within [first, last) or last if all elements satisfy p.
      */
+    static long partitionPoint(LongPredicate predicate, long first, long last) {
+        long left = first, right = last;
+        while (left + 1 < right) {
+            long mid = (left + right) / 2;
+            if (predicate.test(mid)) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return predicate.test(left) ? right : left;
+    }
+
     static int partitionPoint(IntPredicate predicate, int first, int last) {
         int left = first, right = last;
         while (left + 1 < right) {
